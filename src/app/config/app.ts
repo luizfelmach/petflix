e0eacab9cacff2d73b7a4c55a30314efcde88ab8
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-05-14 11:54:15 -0300

    add cors config

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,8 +1,10 @@
 import express, { Express, json } from 'express';
 import { routes } from './routes';
+import cors from 'cors';
 
 const setupApp = async (): Promise<Express> => {
     const app = express();
+    app.use(cors());
     app.use(json());
     routes(app);
     return app;
